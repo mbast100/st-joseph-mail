51a7b925c58f985e5bf3b1b219c882bd7bfb082c
Eric Haggar <eric.haggar@qlik.com>
2021-02-15 18:29:52 -0500

    change lambda message

--- src/services/email.js
+++ src/services/email.js
@@ -5,7 +5,7 @@ module.exports.send = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Your email was successfully sent!',
         input: event,
       },
       null,
