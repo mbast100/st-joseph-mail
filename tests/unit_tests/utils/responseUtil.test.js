4dd06340a4b5585721484a1853aea06b45691f3c
Eric Haggar <erichaggar@gmail.com>
2021-02-28 16:21:07 -0500

    Fix unit test

--- tests/unit_tests/utils/responseUtil.test.js
+++ tests/unit_tests/utils/responseUtil.test.js
@@ -17,12 +17,12 @@ describe('ResponseUtil', () => {
       data = { parameters: "email type" }
     })
 
-    it(`should return response with statusCode ${statusCode}`, async () => {
+    it(`should return response with statusCode 200`, () => {
       const res = responseUtil.buildSuccessfulResponse(data)
       expect(res.statusCode).equal(200)
       expect(res.body).deep.equal(JSON.stringify(data))
     })
-    it(`should return response with statusCode ${statusCode} with no data`, async () => {
+    it(`should return response with statusCode 200 and no data`, () => {
       const res = responseUtil.buildSuccessfulResponse()
       expect(res.statusCode).equal(200)
       expect(res).deep.equal({ statusCode })
@@ -35,7 +35,7 @@ describe('ResponseUtil', () => {
       { statusCode: 500, body: { message: 'Internal server error' } },
     ]
     _.forEach(mockResponses, (mockResponse) => {
-      it(`should return response with statusCode ${mockResponse.statusCode} and body contains ${mockResponse.message}`, async () => {
+      it(`should return response with statusCode ${mockResponse.statusCode} and body contains ${mockResponse.message}`, () => {
         const res = responseUtil.buildErrorResponse(mockResponse.statusCode, mockResponse.body)
         expect(res.statusCode).equal(mockResponse.statusCode)
         expect(res.body).deep.equal(mockResponse.body)
